713743e93514abd3a8d7b0924038a4190bf1321d
tloz <tloz@pop-os.localdomain>
2024-02-24 21:43:49 -0600

    Testing commit from netbeans

---
+++ Proyecto_de_prueba/src/proyecto_de_prueba/Proyecto_de_prueba.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_de_prueba;
+
+/**
+ *
+ * @author tloz
+ */
+public class Proyecto_de_prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Hola Mundo");
+    }
+    
+}
