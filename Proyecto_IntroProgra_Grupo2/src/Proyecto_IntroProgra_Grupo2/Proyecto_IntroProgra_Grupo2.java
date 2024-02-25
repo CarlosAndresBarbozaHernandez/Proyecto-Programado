0c5c9bdd30257bf1cff79232e33d3bf06a7087e4
tloz <tloz@pop-os.localdomain>
2024-02-25 10:25:02 -0600

    Updated Project and file names

---
+++ Proyecto_IntroProgra_Grupo2/src/Proyecto_IntroProgra_Grupo2/Proyecto_IntroProgra_Grupo2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Proyecto_IntroProgra_Grupo2;
+
+/**
+ *
+ * @author tloz
+ */
+public class Proyecto_IntroProgra_Grupo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Hola Mundo");
+
+        System.out.println("Hola mi gente");
+    }
+    
+}
